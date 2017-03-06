719692323d950c08cd5a9ddc7b88615638f6e24c
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-06 16:14:39 +0000

    add a model to store location logs in the database

---
+++ app/src/main/java/com/mignot/kumar/models/LocationEntry.java
@@ -0,0 +1,19 @@
+package com.mignot.kumar.models;
+
+import android.support.annotation.NonNull;
+
+/**
+ * Simple model of a LocationEntry
+ * Fulfils requirements that we log the retrieved location and a timestamp
+ */
+public class LocationEntry {
+  private final Double longitude;
+  private final Double latitude;
+  private final String timestamp;
+
+  public LocationEntry(@NonNull Double lat, @NonNull Double lon, @NonNull String ts) {
+    longitude = lon;
+    latitude = lat;
+    timestamp = ts;
+  }
+}
