b7d92fd4395ebfdfbae3a0733d59b02e4dd60854
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-07 12:36:33 +0000

    add getters to model - don't need them for this exercise but useful for testing

--- app/src/main/java/com/mignot/kumar/indoormap/MainActivity.java
+++ app/src/main/java/com/mignot/kumar/indoormap/MainActivity.java
@@ -3,6 +3,8 @@ package com.mignot.kumar.indoormap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.mignot.kumar.models.LocationEntry;
+
 public class MainActivity extends AppCompatActivity {
 
   @Override
--- app/src/main/java/com/mignot/kumar/models/LocationEntry.java
+++ app/src/main/java/com/mignot/kumar/models/LocationEntry.java
@@ -6,14 +6,29 @@ import android.support.annotation.NonNull;
  * Simple model of a LocationEntry
  * Fulfils requirements that we log the retrieved location and a timestamp
  */
-public class LocationEntry {
+final public class LocationEntry {
   private final Double longitude;
   private final Double latitude;
   private final String timestamp;
 
-  public LocationEntry(@NonNull Double lat, @NonNull Double lon, @NonNull String ts) {
+  public LocationEntry(@NonNull Double lat,
+                       @NonNull Double lon,
+                       @NonNull String ts) {
+
     longitude = lon;
     latitude = lat;
     timestamp = ts;
+  }
+
+  public Double getLongitude() {
+    return longitude;
+  }
+
+  public Double getLatitude() {
+    return latitude;
+  }
+
+  public String getTimestamp() {
+    return timestamp;
   }
 }
