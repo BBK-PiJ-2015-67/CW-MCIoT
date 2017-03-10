aad4b1b2a77f17c15e483a8fb6e3a678af5287c9
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-10 14:40:38 +0000

    document TrackerCallback

--- app/src/main/java/com/mignot/kumar/tracker/TrackerCallback.java
+++ app/src/main/java/com/mignot/kumar/tracker/TrackerCallback.java
@@ -1,5 +1,13 @@
 package com.mignot.kumar.tracker;
 
+/**
+ * Simple interface to define a callback used by the tracker
+ */
 public interface TrackerCallback {
+
+  /**
+   * Executes the callback with the given input
+   * @param input Some input string
+   */
   void execute (String input);
 }
