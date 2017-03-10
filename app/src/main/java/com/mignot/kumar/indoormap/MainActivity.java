f8fb7e477159054bf8e286473d0e95c7c2a8bebe
Selvakumar Kuppusamy <selva.kuppu@gmail.com>
2017-03-10 13:13:53 +0000

    typo

--- app/src/main/java/com/mignot/kumar/indoormap/MainActivity.java
+++ app/src/main/java/com/mignot/kumar/indoormap/MainActivity.java
@@ -7,7 +7,7 @@ public class MainActivity extends SingleFragmentActivity {
 
   @Override
   protected Fragment createFragment() {
-    return new SimpleTrackingFragement();
+    return new SimpleTrackingFragment();
   }
 
   @Override
