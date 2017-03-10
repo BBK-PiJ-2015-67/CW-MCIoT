2bbfa51e9f40fe0aa824f90961df09c65352c4f6
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-10 12:27:48 +0000

    add LocationTracking class

---
+++ app/src/main/java/com/mignot/kumar/tracker/LocationTracking.java
@@ -0,0 +1,71 @@
+package com.mignot.kumar.tracker;
+
+import android.os.Bundle;
+
+import com.indooratlas.android.sdk.IALocation;
+import com.indooratlas.android.sdk.IALocationListener;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.indooratlas.android.sdk.IALocationRequest;
+import com.mignot.kumar.logger.LocationLogger;
+import com.mignot.kumar.models.LocationEntry;
+
+import java.util.Calendar;
+
+/**
+ * Handle location tracking
+ */
+public class LocationTracking {
+  private final long LOG_INTERVAL = 1000;
+  private final LocationLogger mLocationLogger;
+  private final IALocationManager mLocationManager;
+
+  private IALocationListener mLocationListener;
+
+  public LocationTracking (LocationLogger newLogger, IALocationManager newLocMan) {
+    mLocationLogger = newLogger;
+    mLocationManager = newLocMan;
+
+    mLocationListener = new IALocationListener() {
+      @Override
+      public void onLocationChanged(IALocation location) {
+        mLocationLogger.log(new LocationEntry (
+            location.getLatitude(),
+            location.getLongitude(),
+            Calendar.getInstance().toString())
+        );
+      }
+      @Override
+      public void onStatusChanged(String s, int i, Bundle bundle) {
+        // no-op
+      }
+    };
+  }
+
+  /**
+   * Start tracking locations and logging them
+   */
+  public void start() {
+    mLocationManager.requestLocationUpdates(
+        IALocationRequest
+        .create()
+        .setFastestInterval(LOG_INTERVAL),
+        mLocationListener
+    );
+  }
+
+  /**
+   * Stop tracking locations
+   */
+  public void stop() {
+    mLocationManager.removeLocationUpdates(mLocationListener);
+  }
+
+  /**
+   * Clean up any references
+   */
+  public void onDestroy() {
+    mLocationLogger.onDestroy();
+    mLocationManager.destroy();
+    mLocationListener = null;
+  }
+}
