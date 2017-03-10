7445d8e94f95de551d3ed7a69d31c3a2ec5107c6
Laurent Mignot <lmigno01@mail.bbk.ac.uk>
2017-03-10 12:33:31 +0000

    example for Tracker use in MainActivity

--- app/src/main/java/com/mignot/kumar/indoormap/MainActivity.java
+++ app/src/main/java/com/mignot/kumar/indoormap/MainActivity.java
@@ -3,13 +3,34 @@ package com.mignot.kumar.indoormap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+import com.indooratlas.android.sdk.IALocationManager;
+import com.mignot.kumar.logger.FireBaseLocationLogger;
 import com.mignot.kumar.models.LocationEntry;
+import com.mignot.kumar.tracker.LocationTracking;
 
 public class MainActivity extends AppCompatActivity {
+  private final static String DATABASE_REF = "location-logs";
+  private LocationTracking tracker;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+
+    tracker = new LocationTracking (
+        FireBaseLocationLogger.getInstance(FirebaseDatabase.getInstance().getReference(DATABASE_REF)),
+        IALocationManager.create(this)
+    );
+
+    tracker.start();
+
+    tracker.stop();
+  }
+
+  @Override
+  protected void onDestroy() {
+    tracker.onDestroy();
+    super.onDestroy();
   }
 }
